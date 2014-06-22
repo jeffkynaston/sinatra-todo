5f72a232c7192714bba09cf6bfecd0bcba4d3dc1
Jeff Kynaston <kynaston.jeffrey@gmail.com>
2014-06-21 17:48:48 -0700

    added layout & home views, css, now have working mvp

--- todo.rb
+++ todo.rb
@@ -1,3 +1,19 @@
 require_relative 'config/application'
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+# puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+get '/' do
+  @lists = Lists.all.reverse
+  @title = 'All Lists'
+  erb :home
+end
+
+post '/' do
+  n = Lists.new
+  n.name = params[:name]
+  n.description = params[:description]
+  n.created_at = Time.now
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
